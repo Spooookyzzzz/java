be6d5fb84d6f463002166c6c8ea9ecb8e8a9f45f
Spooookyzzzz <107578897+Spooookyzzzz@users.noreply.github.com>
2022-11-21 12:54:07 +0600

    Delete Clock directory

--- Clock/birds/src/com/company/Birds.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Birds implements Flys {
-
-    @Override
-    public void fly() {
-        System.out.println("Bird flies!");
-    }
-}
--- Clock/birds/src/com/company/Sparow.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Sparow extends Birds {
-
-    @Override
-    public void fly() {
-        System.out.println("Sparrow flies!");
-    }
-}
